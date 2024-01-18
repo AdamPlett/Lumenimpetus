710a7646237d2cbc5713dd7ebae3d61136d42cf1
Adam <Adamqplett@gmail.com>
2024-01-18 14:06:11 -0600

    Added shake to unsuccessful grapple crosshair

--- Assets/Scripts/UI/CrosshairManager.cs
+++ Assets/Scripts/UI/CrosshairManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class CrosshairManager : MonoBehaviour
 {
     [Header("Refereneces")]
     public GameObject crosshairGrapple;
     public Image grappleImage;
+
+    [Header("Shake Settings")]
+    public float duration;
+    public float strength;
+    public int virbrato;
+    public float randomness;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,5 +36,9 @@ public class CrosshairManager : MonoBehaviour
         gameObject.SetActive(!grappling);
         crosshairGrapple.SetActive(grappling);
 
+    }
+    public void shakeGrappleCrosshair()
+    {
+        crosshairGrapple.GetComponent<RectTransform>().DOShakeAnchorPos(duration, strength, virbrato, randomness);
     }
 }
