c86c8f83c2b25c8f3ce1b8e567633877953a7ca0
Adam <Adamqplett@gmail.com>
2024-02-01 13:18:03 -0600

    Moving platform fix and WIP player movement on platform
    Fixed moving platform to move by offset instead of two position

--- Assets/Scripts/Environment/MovingPlatformLoop.cs
+++ Assets/Scripts/Environment/MovingPlatformLoop.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 using DG.Tweening;
 public class MovingPlatformLoop : MonoBehaviour
 {
-    [Header("Direction Vector")]
+    [Header("Movement Offset")]
     public float x = 0;
     public float y = 0;
     public float z = 0;
 
+    [Header("Duration")]
     public float duration = 0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 direction = new Vector3 (x, y, z);
-        transform.DOLocalMove(direction, duration)
+        Vector3 direction = new Vector3 (transform.position.x + x, transform.position.y + y, transform.position.z + z);
+        transform.DOMove(direction, duration)
             .SetLoops(-1, LoopType.Yoyo)
             .SetEase(Ease.InOutSine);
     }
---
+++ Assets/Scripts/PlayerController/Movement/MovingPlatformDetector.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GameManager;
+
+public class MovingPlatformDetector : MonoBehaviour
+{
+    public bool isGrounded;
+    public bool check;
+
+    void Update()
+    {
+        isGrounded = gm.pm.grounded;
+        if (!isGrounded)
+        {
+            check = false;
+            if (Input.GetAxisRaw("Horizontal") > .25f || Input.GetAxisRaw("Horizontal") <-.25f || Input.GetAxisRaw("Vertical") > .25f || Input.GetAxisRaw("Vertical") < -.25f)
+            {
+                transform.SetParent(null);
+            } 
+        }
+        if (!check)
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position, Vector3.down, out hit, gm.pm.playerHeight * 0.5f + 0.2f);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("MovingPlatform"))
+                {
+                    transform.SetParent(hit.transform);
+                }
+                else
+                {
+                    transform.SetParent(null);
+                }
+                check = true;
+            }
+        }
+    }
+}
